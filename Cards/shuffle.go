be5d5494fdd58e32f9e6588130ab74db08111c75
gkatsikis <georgioscabro@gmail.com>
2023-09-17 15:08:06 -0400

    create shuffle method

--- Cards/shuffle.go
+++ Cards/shuffle.go
@@ -1,5 +1,18 @@
-// package main
+package main
 
-// func shuffle() deck {
+import {
+	"math/rand"
+	"time"
+}
 
-// }
+func shuffle(d deck) deck {
+	rand.Seed(time.Now().UnixNano())
+
+	numberOfCards := len(d)
+
+	rand.Shuffle(numberOfCards, func(i, j int)) {
+		d[i], d[j] = d[j], d[i]
+	}
+
+	return d
+}
